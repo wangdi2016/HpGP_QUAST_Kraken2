fbd2bd92825b1e7ef725e6fb03ea9b4662414993
Difei Wang <49001003+wangdi2016@users.noreply.github.com>
2026-03-24 20:06:06 -0400

    Add

--- HpGP-Kraken2/results/HpGP/x.sh
+++
@@ -1,8 +0,0 @@
-for f in `cat xx.1`
-do
-
-a=$(tail -n 1 ${f})
-
-echo -e "$f\t$a"  >> all.txt
-
-done
---
+++ HpGP-Kraken2/scripts/summ.sh
@@ -0,0 +1,8 @@
+for f in `cat HpGP.list`
+do
+
+a=$(tail -n 1 ${f})
+
+echo -e "$f\t$a"  >> kraken2_summ.txt
+
+done
